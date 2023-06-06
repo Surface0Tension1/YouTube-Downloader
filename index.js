3c88798cd7d8e1881b913df8370a59cb1902f1ad
Kyle Kavanagh <kkavanagh@ufl.edu>
2023-06-05 21:52:59 -0400

    anything

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var ytdl = require('ytdl-core');
+
+app.listen('4000', function(){
+	console.log("listening on 4000");
+});
+
+app.get('/download', function(req, res) {
+	var link = req.query.url;
+	var format = req.query.format;
+	var quality = req.query.quality;
+
+	video = ytdl(link,{
+		format:format,
+		quality:quality,
+	});
+	video.pipe(res);
+});
